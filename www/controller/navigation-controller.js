52dfe1a3d2957019b79ded90e4721eae03be2dfb
kukacdav <kukacdav@fit.cvut.cz>
2017-01-12 03:40:25 +0900

    Zjednoduseni navigation-controlleru

--- www/controller/navigation-controller.js
+++ www/controller/navigation-controller.js
@@ -9,121 +9,52 @@ document.addEventListener('init', function(event)
     console.log("Navigation handler..");
     var page = event.target;
     if (page.id === 'main-page')
-    {
+    {    
         console.log("N1. Initializing Main Page ");
-        page.querySelector('#create-request-button').onclick = function()
-            {
-                document.querySelector('#pageNavigator')
-                    .pushPage('view/html/create-request-page.html',
-                    {
-                        data:
-                        {
-                            title: 'Zadání requestu'
-                        }
-                    });
-            };
-        page.querySelector('#request-toolbar-button').onclick = function()
-            {
-                document.querySelector('#pageNavigator')
-                    .pushPage('view/html/create-request-page.html',
-                    {
-                        data:
-                        {
-                            title: 'Zadání requestu'
-                        }
-                    });
-            }; 
-        
+        page.querySelector('#create-request-button').onclick = function(){switchPage('view/html/create-request-page.html');};
+        page.querySelector('#request-toolbar-button').onclick = function(){switchPage('view/html/create-request-page.html');};
         page.querySelector('#payment-button').addEventListener('click', showPayments);
         page.querySelector('#request-button').addEventListener('click', showRequests);
-        page.querySelector('#user-detail-button').onclick = function() {
-            document.querySelector('#pageNavigator')
-                    .pushPage('view/html/user-detail-page.html',
-                    {
-                        data:
-                        {
-                            title: 'Zadání requestu'
-                        }
-                    });
-
-        };
+        page.querySelector('#user-detail-button').onclick = function(){switchPage('view/html/user-detail-page.html');};
         showPayments();
     }
     else if (page.id === 'create-request-page')
     {
         console.log("N2. Initializing Create-request-page ");
-        page.querySelector('#define-transaction-button')
-            .onclick = function()
-            {
-                document.querySelector('#pageNavigator')
-                    .pushPage('view/html/define-transaction-page.html',
-                    {
-                        data:
-                        {
-                            title: 'Detail requestu'
-                        }
-                    });
-            };
+        page.querySelector('#define-transaction-button').onclick = function(){switchPage('view/html/define-transaction-page.html');};
     }
     else if (page.id === 'success-submit-page')
     {
         console.log("N3. Initializing submit-success-page ");
-        page.querySelector('#transaction-success-button')
-            .onclick = function()
-            {
-                document.querySelector('#pageNavigator')
-                    .pushPage('view/html/main-page.html',
-                    {
-                        data:
-                        {
-                            title: 'Detail requestu'
-                        }
-                    });
-            };
+        page.querySelector('#transaction-success-button').onclick = function(){switchPage('view/html/main-page.html');};
     }
     else if (page.id === 'define-transaction-page')
     {
         console.log("N4. Initializing define-transaction-page ");
-        page.querySelector('#submit-transaction-button')
-            .onclick = function()
-            {
-                document.querySelector('#pageNavigator')
-                    .pushPage('view/html/success-submit-page.html',
-                    {
-                        data:
-                        {
-                            title: 'Transakce zadána'
-                        }
-                    });
-            };
+        page.querySelector('#submit-transaction-button').onclick = function(){switchPage('view/html/success-submit-page.html');};
     }
     else if (page.id === 'user-detail-page') {
         console.log("N5. Initializing user-detail-page");
         showUserData();
     }
+    else if (page.id === 'transaction-detail-page')
+    {
+        
+    }
 });
 
 
 successfullTransaction = function () {
-    document.querySelector('#pageNavigator')
-                    .pushPage('view/html/success-submit-page.html',
-                    {
-                        data:
-                        {
-                            title: 'Transakce úspěšně zadána'
-                        }
-                    });
+    switchPage(view/html/success-submit-page.html);
 };
 
-showTransactionDetail = function(transactionIndex, transactionType) {
-    console.log(transactionIndex + " " + transactionType);
-    document.querySelector('#pageNavigator')
-                    .pushPage('view/html/transaction-detail-page.html',
-                    {
-                        data:
-                        {
-                            transactionIndex: transactionIndex,
-                            transactionType: transactionType
-                        }
-                    });
+showTransactionDetail = function(event) {
+    console.log("Event:" + $(event).text());
+    console.log("Nav: "+ $(event.target).html());
+    switchPage(view/html/transaction-detail-page.html);
+    composeTransactionDetailPage(event.target);
+};
+
+switchPage = function(target) {
+    document.querySelector('#pageNavigator').pushPage(target);
 };
